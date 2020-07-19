797e165b93a0ab0969f40faf933ef365c69b004d
Dawid Cielicki <gamer@o2.pl>
2020-07-19 12:35:17 +0200

    Initial commit

---
+++ src/main/java/pl/dcielicki/steamutils/SteamutilsApplication.java
@@ -0,0 +1,13 @@
+package pl.dcielicki.steamutils;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SteamutilsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SteamutilsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/dcielicki/steamutils/SteamutilsApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.dcielicki.steamutils;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SteamutilsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
